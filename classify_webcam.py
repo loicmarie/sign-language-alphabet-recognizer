9db07181ecf056ddbb1012fc621cbcb1faa0d061
Simbarashe Timothy Motsi <simbamotsi1@gmail.com>
2017-09-27 15:22:39 +0200

    Update classify_webcam.py
    renamed the files being referenced to output_... because those are the output files from the train module.

--- classify_webcam.py
+++ classify_webcam.py
@@ -31,10 +31,10 @@ def predict(image_data):
 
 # Loads label file, strips off carriage return
 label_lines = [line.rstrip() for line
-                   in tf.gfile.GFile("logs/trained_labels.txt")]
+                   in tf.gfile.GFile("logs/output_labels.txt")]
 
 # Unpersists graph from file
-with tf.gfile.FastGFile("logs/trained_graph.pb", 'rb') as f:
+with tf.gfile.FastGFile("logs/output_graph.pb", 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
     _ = tf.import_graph_def(graph_def, name='')
